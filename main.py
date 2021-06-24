f1612893d21ab73aa288ef521d38ef34a0e832f3
brunofz <brunofabrizio15@gmail.com>
2021-06-24 17:08:16 -0300

    update

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
+# from math import *  # for maths functions
+#
+# # Shape
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+#
+# # Variables & Data Types
+# character_name = "Bruno"
+# character_age = 23
+# isMale = True
+#
+# print("\nThere one was a man named " + character_name + ",")
+# print("He was ", character_age, " years old.")
+#
+# # Strings
+# print("\"Hello\" \"world\"")
+#
+# phrase = "academy"
+#
+# # Concatenation
+# print(phrase + " is cool")
+#
+# print(phrase.upper().isupper())
+#
+# print(len(phrase))  # N characters
+# print(phrase[3])  # index
+#
+# print(phrase.index("d"))
+# print(phrase.replace("de", "Caca"))
+#
+# # Numbers
+# print(5 - 2)
+#
+# my_num = 5
+#
+# print(my_num, " is my number")
+# print(str(my_num) + " is my number")  # convert a number to string
+# print(abs(my_num))  # absolute number
+# print(pow(3, 2))  # powers
+# print(max(4, 6))  # tell us witch number is higher
+# print(min(4, 6))
+# print(round(3, 6))
+#
+# print(floor(3.8))  # round low
+# print(ceil(3.2))  # round up
+# print(sqrt(36))  # square
+#
+#
+# # Getting input from users
+#
+# user_name = input("Enter your name: ")
+# user_age = input("Enter your age: ")
+#
+# print("Hello " + user_name + ", Your age is: " + user_age)
+#
+#
+# # Mad libs game
+#
+# color = input("Enter a color: ")
+# plural_noun = input("Enter a Plural noun: ")
+# celebrity = input("Enter a celebrity: ")
+#
+# print("Roses are " + color)
+# print(plural_noun + " are blue")
+# print("I love " + celebrity)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi - Hola, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Bruno')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
